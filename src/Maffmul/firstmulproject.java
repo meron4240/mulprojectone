c54f7d59dacff7936aecf4361d20efa2709f9b05
meron4240 <meronmaff@gmail.com>
2020-11-01 08:33:32 +0300

    Merry is asazagn

--- src/Maffmul/firstmulproject.java
+++ src/Maffmul/firstmulproject.java
@@ -3,5 +3,7 @@ package Maffmul;
 public class firstmulproject {
     public static void main(String[] args) {
         System.out.println("binny is Smart");
+        System.out.println("java is boring");
+        System.out.println("but trying to make fun");
     }
 }
