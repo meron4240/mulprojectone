e43785a73407f8ee7a9d65fa47f268facf144bf9
meron4240 <meronmaff@gmail.com>
2020-11-01 08:24:13 +0300

    they are good friends.

--- src/Maffmul/firstmulproject.java
+++ src/Maffmul/firstmulproject.java
@@ -3,6 +3,5 @@ package Maffmul;
 public class firstmulproject {
     public static void main(String[] args) {
         System.out.println("binny is Smart");
-        System.out.println("Ariyam");
     }
 }
