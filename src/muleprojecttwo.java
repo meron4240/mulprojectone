4e2ed5aae41fd69911d829bdab8eb984d1e4a413
meron4240 <meronmaff@gmail.com>
2020-11-02 02:24:17 +0300

    Sunday work

---
+++ src/muleprojecttwo.java
@@ -0,0 +1,7 @@
+public class muleprojecttwo {
+    public static void main(String[] args) {
+        System.out.println("Meron mulesoft student");
+        System.out.println("Maths is good subject");
+        System.out.println("One is my favorite number");
+    }
+}
